ed10c652d32db05d99adcc84bcb93b732304909b
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 17:58:20 +0100

    Update Button.tsx

--- alx-project-0x06/components/common/Button.tsx
+++ alx-project-0x06/components/common/Button.tsx
@@ -1,1 +1,15 @@
+interface ButtonProps {
+  buttonLabel: string;
+  buttonBackgroundColor: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ buttonLabel, buttonBackgroundColor }) => {
+  return (
+    <button className={`py-2 px-4 rounded font-semibold text-white bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600`}>
+      {buttonLabel}
+    </button>
+  );
+};
+
+export default Button;
 
