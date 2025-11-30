217851c3ef912b04a16af60219e47451b76f0dd3
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 03:25:05 +0100

    Update _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/layouts/Layout";
-import "@/styles/globals.css"; // Ensure global styles are imported
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
 
