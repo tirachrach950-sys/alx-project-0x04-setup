f9a8f20fc42f93a88fa205072685519986c4404b
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 18:10:36 +0100

    Update CountContext.tsx

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,1 +1,31 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+interface CountContextProps {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+export const CountContext = createContext<CountContextProps | undefined>(undefined);
+
+export const CountProvider = ({ children }: { children: ReactNode }) => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => setCount((c) => c + 1);
+  const decrement = () => setCount((c) => (c > 0 ? c - 1 : 0));
+
+  return (
+    <CountContext.Provider value={{ count, increment, decrement }}>
+      {children}
+    </CountContext.Provider>
+  );
+};
+
+export const useCount = () => {
+  const context = useContext(CountContext);
+  if (!context) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+};
 
