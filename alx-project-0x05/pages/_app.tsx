9e105fa42bb2c6202f59b1c9d5c2f3643d0b1ad8
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 18:13:48 +0100

    Update _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,1 +1,15 @@
+import "@/styles/globals.css"; // Make sure this path is correct
+import type { AppProps } from "next/app";
+import Layout from "@/components/layouts/Layout"; // Make sure Layout exists
+import { CountProvider } from "@/context/CountContext";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
+}
 
