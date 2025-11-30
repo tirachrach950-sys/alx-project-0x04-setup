bb8d6f8004cbb662ce9ee4b2b7330d820410e42c
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 17:56:38 +0100

    Update _app.tsx

--- alx-project-0x06/ pages/_app.tsx
+++ alx-project-0x06/ pages/_app.tsx
@@ -1,16 +1,14 @@
-import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "@/store/store";
+import Header from "@/components/layouts/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Header />
+      <Component {...pageProps} />
     </Provider>
   );
 }
-
