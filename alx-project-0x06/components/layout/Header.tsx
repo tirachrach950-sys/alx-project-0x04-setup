3546226928fe767a0f5433a4dae854b67d03ab0c
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 17:07:11 +0100

    Update Header.tsx

--- alx-project-0x06/components/layout/Header.tsx
+++ alx-project-0x06/components/layout/Header.tsx
@@ -1,28 +1,24 @@
-
 "use client";
 
 import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 import Link from "next/link";
-import { RootState } from "@/redux/store";
 
-export default function Header() {
-  // Access global counter state
-  const counter = useSelector((state: RootState) => state.counter.value);
+const Header = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
 
   return (
-    <header className="w-full p-4 bg-gray-800 text-white flex justify-between">
-      <h1 className="text-xl font-bold">My App</h1>
+    <header className="bg-gray-800 text-white p-4 flex justify-between">
+      <Link href="/">Home</Link>
 
       <nav className="flex gap-4">
-        <Link href="/">Home</Link>
         <Link href="/counter-app">Counter App</Link>
       </nav>
 
-      <div>
-        <span className="font-semibold">Counter: {counter}</span>
-      </div>
+      <span>Counter: {count}</span>
     </header>
   );
-}
+};
 
+export default Header;
 
