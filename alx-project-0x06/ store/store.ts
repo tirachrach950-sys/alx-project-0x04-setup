61fdb430e134346411447fbd080d84d59315ce2c
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 17:01:52 +0100

    Update store.ts

--- alx-project-0x06/ store/store.ts
+++ alx-project-0x06/ store/store.ts
@@ -1,1 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+// Types for Redux
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
