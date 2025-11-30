fabcaf6a78215133831b5ebf7dbf89daceda36cf
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 16:56:37 +0100

    Update Header.tsx

--- alx-project-0x06/components/layout/Header.tsx
+++ alx-project-0x06/components/layout/Header.tsx
@@ -28,4 +28,30 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+"use client";
+
+import { useSelector } from "react-redux";
+import Link from "next/link";
+import { RootState } from "@/redux/store";
+
+export default function Header() {
+  // Access global counter state
+  const counter = useSelector((state: RootState) => state.counter.value);
+
+  return (
+    <header className="w-full p-4 bg-gray-800 text-white flex justify-between">
+      <h1 className="text-xl font-bold">My App</h1>
+
+      <nav className="flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/counter-app">Counter App</Link>
+      </nav>
+
+      <div>
+        <span className="font-semibold">Counter: {counter}</span>
+      </div>
+    </header>
+  );
+}
+
 
